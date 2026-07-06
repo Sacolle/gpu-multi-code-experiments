664677af3e30a9148134e712770ae2fb4d6521bc
sacolle <pedro.h.b.colle@gmail.com>
2026-07-06 13:47:14 -0300

    projeto experimental

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Flake para gerar projeto experimental e fazer o latex";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=26.05";
+    #starvz.url = "github:schnorr/starvz";
+    #starpu.url = "github:Sacolle/nix-starpu";
+  };
+  outputs = { self, /* starvz, starpu, */ nixpkgs }: 
+      let 
+        system = "x86_64-linux";
+        pkgs = import nixpkgs { inherit system; };
+        rEnv = pkgs.rWrapper.override {
+            packages = with pkgs.rPackages; [
+                languageserver
+                numbers
+                lintr
+                here
+                DoE_base
+                FrF2
+                tidyverse
+                janitor
+                #starvz.packages.${system}.starvz
+            ];
+        };
+        /*
+        StarPU = starpu.packages.${system}.default.override {
+            enableCUDA = false;
+            enableTrace = true;
+            extraOptions = [ "--enable-maxcpus=256" "--enable-fxt-max-files=256" ];
+        };
+        myStarvzTools = starvz.packages.${system}.starvzTools.override {
+            inherit StarPU;
+        };*/
+    in 
+  {
+        devShells.${system}.default = pkgs.mkShell { 
+            buildInputs =  [ 
+                rEnv 
+                #myStarvzTools
+            ]; 
+        };
+  };
+}
--- ideal-seg/flake.nix
+++ ideal-seg/flake.nix
@@ -5,12 +5,17 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
         experiments.url = "github:Sacolle/experiments-nix"; 
 
-        star-fletcher.url = "github:Sacolle/Star-Fletcher";
+        star-fletcher.url = "github:Sacolle/Star-Fletcher?ref=CUDA";
+
+        nixpkgs24.url = "github:nixos/nixpkgs/1da52dd49a127ad74486b135898da2cef8c62665";
     };
-    outputs = { self, nixpkgs, experiments, star-fletcher }: 
+    outputs = { self, nixpkgs, experiments, star-fletcher, nixpkgs24 }: 
     let
         system = "x86_64-linux"; 
         pkgs = import nixpkgs { inherit system; };
+
+        pkgs24 = import nixpkgs24 { inherit system; config.allowUnfree = true; };
+        cudaPackages24 = pkgs24.cudaPackages_12_2;
         
         experiment-name = "ideal-block-size-machine";
         scratch-folder = "$SCRATCH/${experiment-name}/$HOSTNAME";
@@ -18,11 +23,12 @@
         str = toString;
         asNum = builtins.fromJSON;
     
-        # before build, validate that star-fletcher is compiling correctly
         my-star-fletcher = star-fletcher.packages.${system}.default.override {
-          enableCUDA = true;
-          enableTrace = false;
-          compileAsRelease = true;
+            cudaPackages = cudaPackages24;
+            stdenv = pkgs24.gcc12Stdenv;
+            enableCUDA = true;
+            enableTrace = false;
+            compileAsRelease = true;
         };
 
         program = "${my-star-fletcher}/bin/star-fletcher";
@@ -37,8 +43,10 @@
                 mkdir -p ${home-folder}
             '';
             
-            bashRunFn = { WithIO,
+            bashRunFn = { 
+                WithIO,
                 BlockSeg,
+                Schedulers,
                 Blocks,
                 Width,
                 AbsorbSize,
@@ -49,7 +57,7 @@
                 ...
             }: 
               let
-                filename = "${BlockSeg}-${WithIO}-${builtins.substring 1 (-1) Blocks}";
+                filename = "${Schedulers}-${BlockSeg}-${WithIO}-${builtins.substring 1 (-1) Blocks}";
                 stdout-file = "${scratch-folder}/stdout-${filename}.out";
                 rsf-file = "${scratch-folder}/out-${filename}.rsf";
                 rsf-at-file = "${rsf-file}@";
@@ -58,6 +66,7 @@
                 OUTPUT_FOLDER=${scratch-folder} \
                 OUTPUT_FILE=${filename} \
                 ENABLE_IO=${WithIO} \
+                STARPU_SCHED=${Schedulers} \
                 ${program} TTI ${Width} ${Width} ${Width} \
                 ${AbsorbSize} 12.5 12.5 12.5 \
                 ${TimeStep} ${TotalTime} ${BlockSeg} ${OutputTime} 2>&1 > ${stdout-file}
